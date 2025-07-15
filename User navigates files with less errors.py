ae61a7dc51a7fae86b0ab5ed8ce06f06b5537c63
edward-penny <RAL-EdwardP@fedextest.onmicrosoft.com>
2025-07-15 10:18:14 +0100

    previous project with less errors

---
+++ User navigates files with less errors.py
@@ -0,0 +1,63 @@
+import subprocess as sp
+import os
+import time as t
+temp = "/home/edward-penny"
+
+answer = ""
+isafile = False
+print("Welcome, here you can choose files and get information about them! Make sure you use the correct capitalisation. press enter to begin")
+input()
+
+directory = "/home/edward-penny"
+#begins the user at home
+while not isafile:
+    choice = ""
+    print("here are the files and folders:")
+    items = sp.run(["ls", directory], stdout = sp.PIPE, text=True)
+    print(f"{items.stdout}\nLeave\n")
+    items = str(items.stdout)
+    if items == "":
+        print("Sorry there is nothing in this folder\n"
+              "Try a different path")
+        directory = "/home/edward-penny"
+        sp.run(["ls", directory])
+
+    #shows all the files in the folder
+    temp2 = directory
+    temp3 = answer
+    answer = input("Where would you like to go?\n")
+    if answer == "Leave":
+        answer = "LeaveY"
+        if "Leave" not in items:
+            directory = str(directory)
+            directory = directory.replace(f"/{directory.rsplit('/', 1)[-1]}", '')
+            print(directory)
+            print("Leaving")
+        elif "Leave" in items:
+            while choice != "1" and choice != "2":
+                choice = input("Would you like to access leave or leave? (1,2)")
+                choice = choice.lower()
+                if choice == "1":
+                    answer = "Leave"
+                elif choice == "2":
+                    directory = str(directory)
+                    directory = directory.replace(f"/{directory.rsplit('/', 1)[-1]}", '')
+                    print(directory)
+                    print("Leaving")
+                else:
+                    print("invalid answer, try again")
+    if answer != "LeaveY":
+        temp = directory
+        directory = f"{directory}/{answer}"
+        #sets the directory to what the user chooses
+        pathexists = os.path.exists(directory)
+        if not pathexists:
+            print("Path does not exist, try again")
+            directory = temp2
+        isafile = os.path.isfile(directory)
+        #checks if the path is a file
+
+        if isafile:
+            size = os.path.getsize(directory)
+            date = t.ctime(os.path.getmtime(directory))
+            print(f"The file's size is {size} bytes, the location is {directory}, and it was last modified on {date}")
